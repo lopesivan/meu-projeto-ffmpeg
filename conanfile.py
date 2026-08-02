576f4931ef4272c5be97cb6226734548af3bb3e1
Ivan Lopes <lopesivan.ufrj@gmail.com>
2026-08-01 22:31:01 -0300

    :style: Atualiza o estilo do arquivo conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -5,10 +5,12 @@ from conan.tools.cmake import cmake_layout
 class ExampleRecipe(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "CMakeDeps", "CMakeToolchain"
+    default_options = {
+        "libx265/*:shared": True,
+    }
 
     def requirements(self):
         self.requires("ffmpeg/8.0")
 
     def layout(self):
         cmake_layout(self)
-
